9fde191a6f8be2dc2487b9414dbe8f79b7df927b
pxnx <loool1912@web.de>
2022-11-03 18:42:12 +0100

    Initial commit

---
+++ app/src/main/java/de/fejuma/impfi/screen/StartFragment.kt
@@ -0,0 +1,64 @@
+package de.fejuma.impfi.screen
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.ViewCompositionStrategy
+import androidx.compose.ui.unit.dp
+import androidx.fragment.app.Fragment
+import de.fejuma.impfi.R
+import de.fejuma.impfi.databinding.ActivityMainBinding
+import de.fejuma.impfi.databinding.FragmentStartBinding
+
+
+class StartFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+
+
+    private var _binding: FragmentStartBinding? = null
+    // This property is only valid between onCreateView and
+// onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentStartBinding.inflate(inflater, container, false)
+        val view = binding.root
+       binding.composeView.apply {
+            // Dispose of the Composition when the view's LifecycleOwner
+            // is destroyed
+            setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
+            setContent {
+                // In Compose world
+                MaterialTheme() {
+                    Button({
+
+                    }){
+                        Text("Hello Compose!")
+                    }
+
+                }
+            }
+        }
+        return view
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+
+
+}
