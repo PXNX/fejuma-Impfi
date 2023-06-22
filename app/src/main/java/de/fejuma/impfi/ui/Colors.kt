270e4591c3ea8139fbd0b5ed1f6884b2d165d653
maxes97 <82040964+maxes97@users.noreply.github.com>
2023-06-22 20:44:00 +0200

    nicht benötigte colors gelöscht

--- app/src/main/java/de/fejuma/impfi/ui/Colors.kt
+++ app/src/main/java/de/fejuma/impfi/ui/Colors.kt
@@ -3,15 +3,8 @@ package de.fejuma.impfi.ui
 import androidx.compose.ui.graphics.Color
 
 val purple_200 = Color(0xFFBB86FC)
-val purple_500 = Color(0xFF6200EE)
-val purple_700 = Color(0xFF3700B3)
-val teal_200 = Color(0xFF03DAC5)
-val teal_700 = Color(0xFF018786)   //todo: use colorResource(
-
 val darkGreen = Color(0xFF219653)
-val lightGreen = Color(0xFF6FCF97)
+
 val lightGray = Color(0xFFE0E0E0)
 val darkGray = Color(0xFF4F4F4F)
-
-val black = Color(0xFF000000)
 val white = Color(0xFFFFFFFF)
